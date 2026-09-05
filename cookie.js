b0ec1c612b6b869a0c2fa3523587d1040d9167eb
Nandita <nanditavarun3@gmail.com>
2026-09-06 01:15:03 +0530

    Update: added actual tips

--- cookie.js
+++ cookie.js
@@ -1,12 +1,25 @@
 const tips = [
-    "Open your textbook. Stare at it. Congratulations, you studied.",
-    "If the chapter looks difficult, close the book.",
-    "Study for 2 minutes. Take a 30 minute break. Perfect balance.",
-    "Highlight everything. Now everything is important.",
-    "Put your textbook under your pillow. Knowledge acquired.",
-    "Read the same sentence five times. Surely it worked.",
-    "Write the date at the top of the page. Very productive.",
-    "If you don't understand the topic, simply understand something else."
+    "If you have an assignment due at 11:59pm, best to start it by 11:58!!!",
+    "Remember to always take 1hour breaks after each 5min study sesh!!!",
+    "Smart harder, not work.",
+    "You don't have to do it today. Give it time, even its due in 4 hours.",
+    "The Pomegranate method is a great way to learn; you open the fruit, ready to devour it wholly, and then abandon it for the next few days!!!",
+    "Live, laugh, load your favourite game instead.",
+    "Always try to apply the first law of motion to your study techniques.",
+    "Just study bruh, it ain't that hard 🙄🙄🙄",
+    "Have faith in God, only he can save you.",
+    "Eat, Sleep, Procrastinate.",
+    "During exam, act like AI: drink lots of water and copy from actually talented people.",
+    "At this point just give up bro 🥀🥀🥀",
+    "Eat your notes and absorb all the info.",
+    "Learning how to read and write is a good place to start 🤓👆",
+    "Have a 5Star, do nothing (not sponsored).",
+    "Reject education, embrace the Ooga Booga.",
+    "Assert dominance over the tough topics by ignoring them.",
+    "Treat your notes like they are drugs. Pile them up and set them on fire like the police does."
+    "Don't worry about failure, you could always change your identity and emigrate to a new country.",
+    "Refresh your topics periodically, preferably once in every blue moon.",
+    "Open your textbook. Stare at it. Congratulations, you have studied 🥳🎉🎉🎉"
 ];
 
 const cookie = document.querySelector(".cookie");
